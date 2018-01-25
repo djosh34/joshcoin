bc2e3126a485691fb31269e92c51a55c90038bd8
djosh34 <35622390+djosh34@users.noreply.github.com>
2018-01-25 15:13:08 +0100

    8

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -95,7 +95,7 @@ public:
         
         while(true)
         {
-        genesis.nNonce = genesis.nNonce + 1;
+        ++genesis.nNonce;
         consensus.hashGenesisBlock = genesis.GetHash();
         fprintf(stderr, "Nonce: %s  hashGenesisBlock: %s\n",genesis.nNonce.ToString().c_str(), consensus.hashGenesisBlock.ToString().c_str());
         
